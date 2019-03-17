ee03034ce18f3a0cd4803a4b39e1bf86cedcc6ed
Chris Yates <cmyates48@gmail.com>
2019-03-17 15:47:31 -0700

    Updates

---
+++ precache-manifest.c99aa2e3b01f82a30a262ef766c46505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "d9058e56506b421775e4",
+    "url": "/portfolio/static/js/main.8c228b8b.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/portfolio/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "d9058e56506b421775e4",
+    "url": "/portfolio/static/css/main.4020ecaa.chunk.css"
+  },
+  {
+    "revision": "6d402046108b15f5465b739c4cd2dd73",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6cf1a2978ce2ec3e171e34ab152f0639.js"
+  "/portfolio/precache-manifest.c99aa2e3b01f82a30a262ef766c46505.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
