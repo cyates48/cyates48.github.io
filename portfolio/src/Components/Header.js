f4c4332246846f53c6ac604ba4ca2c77449ce873
Chris Yates <cmyates48@gmail.com>
2019-03-19 19:49:09 -0700

    created header file, to test environment

---
+++ portfolio/src/Components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import './App.css';
+
+class Header extends Component {
+    state = { };
+
+  render() {
+    return (
+      <header className="Header">
+        <p> First p tag lalala </p>
+      </header>
+    );
+  }
+}
+
+export default Header;
